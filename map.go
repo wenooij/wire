94461bd0f926bb46e365c2dec69dabe4b5a88cea
Wes de Nooij <142677213+wenooij@users.noreply.github.com>
2024-07-08 16:41:02 +0000

    Proto: Add Table proto for comparable multimaps

--- map.go
+++ map.go
@@ -14,7 +14,7 @@ func (m MapVal[K, V]) Sort(cmp func(a, b Tup2Val[K, V]) int) {
 
 // Compact sorts the map entries and compacts equal keys.
 //
-// Compact eliminates multimap properties.
+// Compact removes multiple values for the same key, only keeping the first.
 func (m *MapVal[K, V]) Compact(cmp func(a, b Tup2Val[K, V]) int) {
 	m.Sort(cmp)
 	*m = slices.CompactFunc(*m, func(a, b Tup2Val[K, V]) bool { return a.E0 == b.E0 })
@@ -26,4 +26,27 @@ func RawMap[K comparable, V any](key Proto[K], val Proto[V]) ProtoRanger[[]Tup2V
 
 func Map[K comparable, V any](key Proto[K], val Proto[V]) ProtoMakeRanger[[]Tup2Val[K, V], SpanElem[[]Tup2Val[K, V]], Tup2Val[K, V]] {
 	return spanMakeRanger[[]Tup2Val[K, V], Tup2Val[K, V]](RawMap(key, val))
+}
+
+type TableVal[K, V comparable] []Tup2Val[K, V]
+
+// Sort the Table based on the given cmp function which may implement multimap sorting.
+func (m TableVal[K, V]) Sort(cmp func(a, b Tup2Val[K, V]) int) {
+	slices.SortFunc(m, cmp)
+}
+
+// Compact sorts the Table entries and compacts equal keys and values.
+//
+// Compact removes duplicate (K, V) entries.
+func (m *TableVal[K, V]) Compact(cmp func(a, b Tup2Val[K, V]) int) {
+	m.Sort(cmp)
+	*m = slices.CompactFunc(*m, func(a, b Tup2Val[K, V]) bool { return a.E0 == b.E0 && a.E1 == b.E1 })
+}
+
+func RawTable[K, V comparable](key Proto[K], val Proto[V]) ProtoRanger[[]Tup2Val[K, V], Tup2Val[K, V]] {
+	return RawSeq(Tup2(key, val))
+}
+
+func Table[K, V comparable](key Proto[K], val Proto[V]) ProtoMakeRanger[[]Tup2Val[K, V], SpanElem[[]Tup2Val[K, V]], Tup2Val[K, V]] {
+	return spanMakeRanger[[]Tup2Val[K, V], Tup2Val[K, V]](RawTable(key, val))
 }
