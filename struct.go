986f4a1f313b70c49a7c7bb85b78343410e24630
Wes de Nooij <142677213+wenooij@users.noreply.github.com>
2024-07-08 00:53:03 +0000

    Struct: Fixes to add a Span, RawStruct, cleanup

--- struct.go
+++ struct.go
@@ -2,71 +2,28 @@ package wire
 
 import (
 	"fmt"
-	"slices"
 )
 
 var anyRawSpan = Span(Any(Raw))
 
-type StructVal map[uint64]SpanElem[any]
+func RawStruct(fields map[uint64]Proto[any]) Proto[[]FieldVal[any]] { return Seq(structField(fields)) }
 
 // Struct enables coding based on a field-numbers-to-Proto mapping.
 //
 // Reading unknown fields will result in a Field of type Raw ([]byte).
 // To appease the generics, the Field type T must be made any.
 // See Any for help working with Any Protos.
-func Struct(fields map[uint64]Proto[any]) Proto[StructVal] {
-	return proto[StructVal]{
-		read:  readStruct(fields),
-		write: writeStruct(fields),
-		size:  sizeStruct(fields),
-	}
+func Struct(fields map[uint64]Proto[any]) Proto[SpanElem[[]FieldVal[any]]] {
+	return Span(RawStruct(fields))
 }
 
-func readStruct(protos map[uint64]Proto[any]) func(Reader) (StructVal, error) {
-	visitSeq := VisitSeq(fieldMapper(protos))
-	return func(r Reader) (StructVal, error) {
-		s := make(StructVal, 4)
-		visitSeq(r, func(field FieldVal[any]) error {
-			s[field.Num()] = field.E1
-			return nil
-		})
-		return s, nil
-	}
-}
-
-func writeStruct(protos map[uint64]Proto[any]) func(Writer, StructVal) error {
-	fieldWriter := fieldMapper(protos)
-	return func(w Writer, s StructVal) error {
-		keys := make([]uint64, 0, len(s))
-		for k := range s {
-			keys = append(keys, k)
-		}
-		slices.Sort(keys)
-		for _, k := range keys {
-			if err := fieldWriter.Write(w, FieldVal[any]{k, s[k]}); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-}
-
-func sizeStruct(protos map[uint64]Proto[any]) func(StructVal) uint64 {
-	return func(s StructVal) uint64 {
-		var size uint64
-		for k, v := range s {
-			proto, ok := protos[k]
-			if !ok {
-				panic(fmt.Errorf("unknown field: %d", k))
-			}
-			size += proto.Size(v)
-		}
-		return size
-	}
+func MakeStruct(fields map[uint64]Proto[any]) func([]FieldVal[any]) SpanElem[[]FieldVal[any]] {
+	rawStruct := RawStruct(fields)
+	return func(fields []FieldVal[any]) SpanElem[[]FieldVal[any]] { return MakeSpan(rawStruct)(fields) }
 }
 
 func VisitStruct(fields map[uint64]Proto[any]) func(Reader, func(FieldVal[any]) error) error {
-	visitSeq := VisitSeq(fieldMapper(fields))
+	visitSeq := VisitSeq(structField(fields))
 	return func(r Reader, f func(FieldVal[any]) error) error {
 		return visitSeq(r, func(field FieldVal[any]) error {
 			if err := f(FieldVal[any]{field.Num(), field.E1}); err != nil {
@@ -80,15 +37,15 @@ func VisitStruct(fields map[uint64]Proto[any]) func(Reader, func(FieldVal[any]) 
 	}
 }
 
-func fieldMapper(p map[uint64]Proto[any]) Proto[FieldVal[any]] {
+func structField(p map[uint64]Proto[any]) Proto[FieldVal[any]] {
 	return proto[FieldVal[any]]{
-		read:  readFields(p),
-		write: writeFields(p),
-		size:  sizeFields(p),
+		read:  readStructField(p),
+		write: writeStructField(p),
+		size:  sizeStructField(p),
 	}
 }
 
-func readFields(protos map[uint64]Proto[any]) func(Reader) (FieldVal[any], error) {
+func readStructField(protos map[uint64]Proto[any]) func(Reader) (FieldVal[any], error) {
 	numReader := Uvarint64.Read
 	spanReaders := make(map[uint64]func(Reader) (SpanElem[any], error), len(protos))
 	for num, proto := range protos {
@@ -112,7 +69,7 @@ func readFields(protos map[uint64]Proto[any]) func(Reader) (FieldVal[any], error
 	}
 }
 
-func writeFields(proto map[uint64]Proto[any]) func(Writer, FieldVal[any]) error {
+func writeStructField(proto map[uint64]Proto[any]) func(Writer, FieldVal[any]) error {
 	writeSpans := make(map[uint64]func(Writer, SpanElem[any]) error, len(proto))
 	for num, fns := range proto {
 		writeSpans[num] = writeSpan(fns)
@@ -129,7 +86,7 @@ func writeFields(proto map[uint64]Proto[any]) func(Writer, FieldVal[any]) error 
 	}
 }
 
-func sizeFields(protos map[uint64]Proto[any]) func(FieldVal[any]) uint64 {
+func sizeStructField(protos map[uint64]Proto[any]) func(FieldVal[any]) uint64 {
 	return func(field FieldVal[any]) uint64 {
 		p, ok := protos[field.Num()]
 		if !ok {
--- struct_test.go
+++ struct_test.go
@@ -9,20 +9,18 @@ import (
 
 func TestWriteStruct(t *testing.T) {
 	var b bytes.Buffer
-	s := Struct(
-		map[uint64]Proto[any]{
-			1: Any(RawString),
-			2: Any(Uvarint64),
-		},
-	)
-	if err := s.Write(&b, StructVal{
-		1: MakeAnySpan(RawString)("a"),
-		2: MakeAnySpan(Uvarint64)(uint64(123)),
-	}); err != nil {
+	fields := map[uint64]Proto[any]{
+		1: Any(RawString),
+		2: Any(Uvarint64),
+	}
+	if err := Struct(fields).Write(&b, MakeStruct(fields)([]FieldVal[any]{
+		MakeAnyField(RawString)(1, "a"),
+		MakeAnyField(Uvarint64)(2, 123),
+	})); err != nil {
 		t.Fatal(err)
 	}
 	got := b.String()
-	want := "\x01\x01a\x02\x01{"
+	want := "\x06\x01\x01a\x02\x01{"
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("TestWriteStruct(): got diff:\n%s", diff)
 	}
